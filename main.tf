6570c8d87ff242be7e15dbeb74d65070219c787d
J-odo <mijulius30@gmail.com>
2023-02-28 00:03:01 -0600

    added some instances

--- main.tf
+++ main.tf
@@ -3,6 +3,33 @@ resource "aws_instance" "test-1" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Jenkins-test"
+    Name = "joy"
+  }
+}
+
+resource "aws_instance" "test-2" {
+  ami           = "ami-0dfcb1ef8550277af"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "faith"
+  }
+}
+
+resource "aws_instance" "test-3" {
+  ami           = "ami-0dfcb1ef8550277af"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "peace"
+  }
+}
+
+resource "aws_instance" "test-4" {
+  ami           = "ami-0dfcb1ef8550277af"
+  instance_type = "love"
+
+  tags = {
+    Name = "happiness"
   }
 }
